f1122116ccaf6bd11984d52e1bb6161a44e0924a
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-18 11:15:18 +0000

    update on slotmechine

--- main game/slotmachine.py
+++ main game/slotmachine.py
@@ -1,6 +1,6 @@
 import random
 from time import sleep
-
+from colorama import Fore
 list1 = ["🍒", "🍋", "🍊", "🍉", "🔔", "💎"]
 
 
@@ -16,14 +16,14 @@ def check_win(result):
     if "🍋" in result:
         return 1 
     
-    return 0 
+    return 0
 
 def main():
     bank = 100
     print("Welcome to the slot machine!")
     print(f"You have {bank} tokens.")
     while bank > 0:
-        choice1 = input("Press Enter to pull the lever or type 'quit' to stop playing:").lower()
+        choice1 = input(Fore.WHITE + "Press Enter to pull the lever or type 'quit' to stop playing:").lower()
         if choice1 == 'quit':
             print("Thanks for playing! See you next time.")
             break
@@ -46,9 +46,9 @@ def main():
                 print("you win")
             print(f"Your new balance: {bank} tokens")
         else:
-            print("Invalid input. Please press Enter to spin or type 'quit' to stop.")
+            print(Fore.RED + "Invalid input. Please press Enter to spin or type 'quit' to stop.")
     if bank <= 0:
-        print("You're out of tokens. Game over!")
+        print(Fore.RED + "You're out of tokens. Game over!")
 
 if __name__ == "__main__":
     main()
