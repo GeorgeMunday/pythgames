6a7385a782ed452a191a5f31e9ccdb1884d998c0
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-19 16:00:19 +0000

    update

--- main game/roulette.py
+++ main game/roulette.py
@@ -1,5 +1,16 @@
+from time import sleep
+from colorama import Fore
+import random
+def spin ():
+    return random.randint(0,36)
+
+
+
+
+
+
+
 def main():
-    print("hello")
-    exit()
+    print("welcome to roulette you have")
 if __name__ == "__main__":
     main()
