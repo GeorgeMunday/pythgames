82ec67b33b4367024a44ce018e1c892fc30b9716
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2025-02-07 13:24:16 +0000

    blackjack

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # pythgames
 Games that I have made, all combined into one system.
 
-Everything was added at the start on 18/12/2024, and I have been developing it ever since.
+Everything was added at the start on 18/12/2024.
 
 i have used chat GPT to add comments to my code
--- main game/blackjack.py
+++ main game/blackjack.py
@@ -1,13 +1,54 @@
 import random
 
 def card_gen():
-    print("")
-    return 
-
-
+    return random.randint(2,11)
+    
+def dealer_gen():
+    dealer_card1= card_gen()
+    dealer_card2= card_gen()
+    temp_card= card_gen()
+    total = dealer_card1 + dealer_card2
+    print(f"dealer has {dealer_card1} and {dealer_card2 } this totals to {total}")
+    while 21 > total < 17:
+        total = total + temp_card
+        print(f"then he got {temp_card} witch totals to {total} ")
+        if total == 20 or total == 19:
+            break
+    dealer_total = total
+    if total > 21:
+        return "bust"
+    return dealer_total
 
 def main():
-    print("welcome to blackjack!!")
+    card1= card_gen()
+    card2= card_gen()
+    temp_card= card_gen()
+    total = card1 + card2
+    choice = "hit"
+    print(f"your cards are {card1} and {card2} this totals to {total}")
+    while choice == "hit" and total < 21:
+        choice = input("do you want to hit or hold")
+        if choice == "hit":
+            total = total + temp_card
+            print(total)
+            continue
+    dealertotal = dealer_gen()
+    if total > 21:
+        print("you bust")
+    else:
+        print(f"dealer has {dealertotal}")
+        if total > dealertotal:
+            print("you win")
+        elif total == dealertotal:
+            print("you lose")
+        else:
+            print("you lose")
+    
+        
+        
+    
+    
+
     
 if __name__ == "__main__":
     main()
